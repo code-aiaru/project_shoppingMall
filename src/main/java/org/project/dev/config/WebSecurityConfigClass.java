20278b0befad1a25b108a7aa180d33fc7560a6dd
catfather <catfather49@gmail.com>
2023-08-14 13:07:58 +0900

    config/WebSecurityConfigClass , AdminController

---
+++ src/main/java/org/project/dev/config/WebSecurityConfigClass.java
@@ -0,0 +1,34 @@
+package org.project.dev.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class WebSecurityConfigClass {
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+
+        http.formLogin()
+                .loginPage("/index/login")
+                .loginProcessingUrl("index")
+                .usernameParameter("email")
+                .passwordParameter("password")
+                .defaultSuccessUrl("/index") //성곻시
+                .failureForwardUrl("/index/login"); //실패시
+        http.logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/index/logout"))
+                .logoutSuccessUrl("/index");
+
+        return http.build();
+    }
+}
--- src/main/java/org/project/dev/controller/HomeController.java
+++ src/main/java/org/project/dev/controller/HomeController.java
@@ -27,10 +27,18 @@ public class HomeController {
      3. 필수 데이터
      4. 기타
      */
-    @GetMapping
-    @ResponseBody
+
+
+    /*
+    Todo
+     1. catfather@49gamil.com
+     2. 이 사이트를 접속하면 제일 처음 요청이 들어오는 컨트롤러입니다
+     3. 없음
+     4. 없음
+     */
+    @GetMapping("/index")
     public String index(Model model){
 
-        return "/index";
+        return "index";
     }
 }
