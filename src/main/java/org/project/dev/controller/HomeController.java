0e7daa21c4b5598e257bc1c67fd2b87273013b09
catfather <catfather49@gmail.com>
2023-08-14 12:47:30 +0900

    adminController add

--- src/main/java/org/project/dev/controller/HomeController.java
+++ src/main/java/org/project/dev/controller/HomeController.java
@@ -1,8 +1,19 @@
 package org.project.dev.controller;
 
+import org.project.dev.dto.MemberDto;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+/*
+
+ */
 
 @Controller
 @RequestMapping("/")
@@ -10,13 +21,16 @@ public class HomeController {
 
 
     /*
-    TODO : 만든 사람 (이메일 주소나 영문명)
-       메소드 설명 ~
-        필수 데이터 ~
+    Todo
+     1. 코드 작성자 (이메일 기재)
+     2. 간략한 기능 설명
+     3. 필수 데이터
+     4. 기타
      */
     @GetMapping
-    public String index(){
+    @ResponseBody
+    public String index(Model model){
 
-        return "index";
+        return "/index";
     }
 }
