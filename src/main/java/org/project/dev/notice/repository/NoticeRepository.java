303388624534dcac14cdd2d214db648edd801ec5
yejin <rladpwls1843@gmail.com>
2023-09-09 02:16:26 +0900

    notice 오류 수정

--- src/main/java/org/project/dev/notice/repository/NoticeRepository.java
+++ src/main/java/org/project/dev/notice/repository/NoticeRepository.java
@@ -10,19 +10,15 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface NoticeRepository extends JpaRepository<NoticeEntity, Long> {
 
-    List<NoticeEntity> findByNoticeTitleContaining(String title); // search - title을 포함하고 있는
-    List<NoticeEntity> findByNoticeContentContaining(String content); // search - content를 포함하고 있는
-    List<NoticeEntity> findByNotWriterContaining(String writer); // search - writer을 포함하고 있는
-    Page<NoticeEntity> findByNotType(String type, Pageable pageable); // list - type에 해당하는
+    Page<NoticeEntity> findByNoticeTitleContaining(Pageable pageable, String title); // paging & list & search -> title
+    Page<NoticeEntity> findByNoticeContentContaining(Pageable pageable, String content); // // paging & list & search -> content
+    Page<NoticeEntity> findByNotType(String type, Pageable pageable); // paging & list -> type
 
     @Modifying(clearAutomatically = true)
     @Query(value = "UPDATE notice n set n.not_hit = n.not_hit + 1 where n.not_id = :id" , nativeQuery = true)
     void NoticeHit(@Param("id") Long id);
 
 }
-
