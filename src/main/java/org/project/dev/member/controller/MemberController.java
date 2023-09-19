4c22396a3b66dc3b3fb490c4e4ff113fa41856e6
songwc3 <songwc3@gmail.com>
2023-09-19 17:33:22 +0900

    자잘한 수정

--- src/main/java/org/project/dev/member/controller/MemberController.java
+++ src/main/java/org/project/dev/member/controller/MemberController.java
@@ -125,111 +125,6 @@ public class MemberController {
     }
 
     // Read_paging - 회원 목록 조회
-//    @GetMapping("/pagingList") // page=0 -> DB     // 페이지수, 한페이지 보이는View수 , 정렬
-//    public String getPagingList(@PageableDefault(page = 0, size = 2, sort = "memberId",
-//                                direction = Sort.Direction.DESC) Pageable pageable, Model model,
-//                                @AuthenticationPrincipal MyUserDetails myUserDetails){
-//
-//        // *** Page<>  Pageable
-//        Page<MemberDto> memberList = memberService.memberPagingList(pageable);
-//
-//        long totalCount = memberList.getTotalElements();
-//        int pageSize = memberList.getSize();
-//
-//        // 총 글수 17
-//        // 한페이지 당 size 3
-//        // 총페이수 6
-//        // blockNum=3
-//        //1  2  3    -> 3 3 3
-//        //4  5  6    -> 3 3 2
-//        // 블록의 첫페지이 지
-//        // 블록이 3일 경우     123 -> 1, 456  -> 4 , 789 -> 7
-//        int nowPage = memberList.getNumber(); // 현재 페이지
-//        int totalPage = memberList.getTotalPages(); // 총 페이지 수
-//        int blockNum = 3;
-//
-//        // Math.floor -> 올림
-//        int startPage =
-//                (int)((Math.floor(nowPage/blockNum)*blockNum)+1 <= totalPage ? (Math.floor(nowPage/blockNum)*blockNum)+1 : totalPage);
-//        // 블록의 마지막 페이지
-//        // 블록이 3일 경우      123 -> 3, 456  -> 5 , 789 -> 9
-//        // 시작페이지+블록-1> 전체 페이지 -> 마지막페이지숫자(시작페이지+블록-1)
-//        int endPage = (startPage + blockNum-1 < totalPage ? startPage + blockNum-1 : totalPage);
-//
-//        for(int i=startPage;i<=endPage;i++){
-//            System.out.print(i+" , ");
-//        }
-//
-//        model.addAttribute("startPage", startPage);
-//        model.addAttribute("endPage", endPage);
-//
-//        if (!memberList.isEmpty()) {
-//            model.addAttribute("memberList", memberList);
-//            model.addAttribute("myUserDetails", myUserDetails);
-//            return "member/pagingList";
-//        }
-//        return "member/pagingList";
-//    }
-//
-////    // 검색
-//    @GetMapping("/search")                             // 요청 정보 없으면 그냥 실행
-//    public String getSearch(
-//            @RequestParam(value = "subject", required = false) String subject,
-//            @RequestParam(value = "search", required = false) String search,
-//            @AuthenticationPrincipal MyUserDetails myUserDetails, Model model){
-//
-//        List<MemberDto> memberList=memberService.searchMemberList(subject, search);
-//
-//        if(!memberList.isEmpty()){
-//            model.addAttribute("memberList",memberList);
-//            model.addAttribute("myUserDetails",myUserDetails);
-//
-////            return "member/pagingList";
-//            return "member/memberList";
-//        }
-//        System.out.println("조회할 목록이 없다");
-////        return "redirect:/member/pagingList";
-//        return "redirect:/member/memberList";
-//    }
-
-    // 페이징, 검색 / size 바꿔야함
-//    @GetMapping("/pagingList")
-//    public String getMemberList(
-//            @PageableDefault(page=0, size=7, sort = "memberId", direction = Sort.Direction.DESC) Pageable pageable,
-//            Model model,
-//            @RequestParam(value = "subject", required = false) String subject,
-//            @RequestParam(value = "search", required = false) String search,
-//            @AuthenticationPrincipal MyUserDetails myUserDetails
-//    ) {
-//
-//        MemberDto member = memberService.detailMember(myUserDetails.getMemberEntity().getMemberId());
-//        String memberImageUrl = imageService.findImage(member.getMemberEmail()).getImageUrl();
-//
-//        Page<MemberDto> memberList = memberService.memberList(pageable, subject, search, myUserDetails);
-//
-//        Long totalCount = memberList.getTotalElements();
-//        int totalPage = memberList.getTotalPages();
-//        int pageSize = memberList.getSize();
-//        int nowPage = memberList.getNumber();
-//        int blockNum = 10;
-//
-//        int startPage = (int) ((Math.floor(nowPage / blockNum) * blockNum) + 1 <= totalPage ?
-//                (Math.floor(nowPage / blockNum) * blockNum) + 1 : totalPage);
-//        int endPage = (startPage + blockNum - 1 < totalPage ? startPage + blockNum - 1 : totalPage);
-//
-//        if (!memberList.isEmpty()) {
-//            model.addAttribute("memberList", memberList);
-//            model.addAttribute("startPage", startPage);
-//            model.addAttribute("endPage", endPage);
-//            model.addAttribute("myUserDetails", myUserDetails);
-//            model.addAttribute("memberImageUrl", memberImageUrl);
-//
-//            return "member/pagingList"; // 이름 바꿔주기(memberList 지우고 memberList로)
-//        }
-//        System.out.println("조회할 문의사항이 없다.");
-//
-//        return "redirect:/member/pagingList?page=0&subject=&search=";
-//    }
     @GetMapping("/pagingList")
     public String getMemberList(
             @PageableDefault(page=0, size=7, sort = "memberId", direction = Sort.Direction.DESC) Pageable pageable,
@@ -489,8 +384,7 @@ public class MemberController {
             Model model,
             @RequestParam(value = "select", required = false) String inquirySelect,
             @RequestParam(value = "search", required = false) String inquirySearch,
-            @AuthenticationPrincipal MyUserDetails myUserDetails
-    ) {
+            @AuthenticationPrincipal MyUserDetails myUserDetails) {
 
         if (myUserDetails != null) {
             MemberDto member = memberService.detailMember(myUserDetails.getMemberEntity().getMemberId());
