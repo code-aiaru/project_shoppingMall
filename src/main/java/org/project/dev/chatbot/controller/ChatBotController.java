55b9282aa2bb349e398baeff9c7c2dea5ab31466
jibgagohipda <bin7719@daum.net>
2023-09-19 12:50:18 +0900

    asda

--- src/main/java/org/project/dev/chatbot/controller/ChatBotController.java
+++ src/main/java/org/project/dev/chatbot/controller/ChatBotController.java
@@ -1,12 +1,16 @@
 package org.project.dev.chatbot.controller;
 
 import lombok.RequiredArgsConstructor;
+import net.bytebuddy.asm.Advice;
 import org.project.dev.chatbot.message.BotMessage;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Controller
 @RequiredArgsConstructor
@@ -20,8 +24,15 @@ public class ChatBotController {
 
     @MessageMapping("/hello")
     @SendTo("/topic/greetings")
-    public BotMessage chatSt(){
-        return null;
+    public BotMessage chatSt() throws Exception{
+        Thread.sleep(50);
+        LocalDateTime today = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy년 MM월 dd일");
+        String formattedDay = today.format(formatter);
+        String formattedTime = today.format(DateTimeFormatter.ofPattern("a H:mm"));
+
+        // 처음 실행되는 -> 답장문
+        return new BotMessage("");
     }
 
 }
