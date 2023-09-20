8a0f8e7244fa3cd6b3c34ff1ad7559c460e68ea9
catfather49 <catfather49@gmail.com>
2023-09-20 10:08:03 +0900

    쉘스크립트 변경

--- start.sh
+++ start.sh
@@ -15,7 +15,7 @@ cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
 
 # jar 파일 실행
 echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
-nohup java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
+sudo nohup java -jar $JAR_FILE &
 
 
 
