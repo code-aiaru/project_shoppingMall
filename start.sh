e4e5570ff7259bd5d10f95e8feaa03c3f843fed0
catfather49 <catfather49@gmail.com>
2023-09-20 14:05:16 +0900

    commit

--- start.sh
+++ start.sh
@@ -9,16 +9,13 @@ DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
 TIME_NOW=$(date +%c)
 
-./gradlew build
-nohup java -jar build/libs/dev-0.0.1-SNAPSHOT.jar
+# build 파일 복사
+echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
+cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
 
-## build 파일 복사
-#echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
-#cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
-#
-## jar 파일 실행
-#echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
-#sudo nohup java -jar $JAR_FILE &
-#
-#CURRENT_PID=$(pgrep -f $JAR_FILE)
-#echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
+# jar 파일 실행
+echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
+sudo nohup java -jar $JAR_FILE &
+
+CURRENT_PID=$(pgrep -f $JAR_FILE)
+echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
