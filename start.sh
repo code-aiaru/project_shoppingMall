d66f579045dd2fea4de1e4007ed43f3e965926eb
catfather49 <catfather49@gmail.com>
2023-09-20 16:49:48 +0900

    commit2

--- start.sh
+++ start.sh
@@ -1,24 +1,59 @@
-#!/usr/bin/env bash
-APP_NAME="dev-0.0.1-SNAPSHOT.jar"
-PROJECT_ROOT="/home/ubuntu/app/build/libs"
-JAR_FILE="$PROJECT_ROOT/dev-0.0.1-SNAPSHOT.jar"
+##!/usr/bin/env bash
+#APP_NAME="dev-0.0.1-SNAPSHOT.jar"
+#PROJECT_ROOT="/home/ubuntu/app/build/libs"
+#JAR_FILE="$PROJECT_ROOT/dev-0.0.1-SNAPSHOT.jar"
+#
+#APP_LOG="$PROJECT_ROOT/application.log"
+#ERROR_LOG="$PROJECT_ROOT/error.log"
+#DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
+#
+#TIME_NOW=$(date +%c)
+#
+#CURRENT_PID=$(pgrep -f $APP_NAME)
+#
+#if [ -z $CURRENT_PID ]; then
+#  echo "현재 실행중인 애플리케이션이 없습니다"
+#else
+#  echo "실행중인애플리케이션 종료 "
+#  sudo kill -15 $JAR_FILE
+#fi
+#
+#
+## jar 파일 실행
+#echo "파일 실행"
+#nohup java -jar $JAR_FILE
 
-APP_LOG="$PROJECT_ROOT/application.log"
-ERROR_LOG="$PROJECT_ROOT/error.log"
-DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
+#!/usr/bin/env bash
+
+PROJECT_ROOT="/home/ubuntu/app/build/libs"
+JAR_FILE="$PROJECT_ROOT dev-0.0.1-SNAPSHOT.jar"
+
+#APP_LOG="$PROJECT_ROOT/application.log"
+#ERROR_LOG="$PROJECT_ROOT/error.log"
+#DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
 TIME_NOW=$(date +%c)
 
-CURRENT_PID=$(pgrep -f $APP_NAME)
 
+CURRENT_PID=$(pgrep -f $JAR_FILE)
+
+# 프로세스가 켜져 있으면 종료
 if [ -z $CURRENT_PID ]; then
   echo "현재 실행중인 애플리케이션이 없습니다"
 else
-  echo "실행중인애플리케이션 종료 "
-  sudo kill -15 $JAR_FILE
+  echo "실행중인 애플리케이션 종료 "
+  sudo kill -15 $CURRENT_PID
 fi
 
 
+# build 파일 복사
+#echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
+#cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
+
 # jar 파일 실행
-echo "파일 실행"
-nohup java -jar $JAR_FILE
+#echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
+./gradlew build
+echo "jar 빌드"
+sudo nohup java -jar $JAR_FILE &
+
+#CURRENT_PID=$(pgrep -f $JAR_FILE)
