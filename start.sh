92829e0db3270eb88b12890695183369b58abae1
dlwlgns <fhraoswlgns@naver.com>
2023-09-21 00:34:17 +0900

    commit

--- start.sh
+++ start.sh
@@ -6,21 +6,12 @@ JAR_FILE="$PROJECT_ROOT/dev-0.0.1-SNAPSHOT.jar"
 
 CURRENT_PID=$(pgrep -f $JAR_FILE)
 
-echo "$CURRENT_PID"
-
-# 프로세스가 켜져 있으면 종료
-if [ -z $CURRENT_PID ]; then
-  echo "현재 실행중인 애플리케이션이 없습니다"
-else
-  echo "실행 중인 애플리케이션 종료 "
-  kill -9 $CURRENT_PID
-fi
-
 echo "파일 실행"
 
 # jar 파일 실행
-CURRENT_PID=$(pgrep -f $JAR_FILE)
 echo "$CURRENT_PID"
+
+echo "$JAR_FILE"
 
 nohup java -jar $JAR_FILE
 
