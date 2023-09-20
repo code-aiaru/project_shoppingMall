f215599909a2368d4003c81faa2c95ea0073a9dc
catfather49 <catfather49@gmail.com>
2023-09-20 15:50:17 +0900

    commit2

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+APP_NAME="dev-0.0.1-SNAPSHOT.jar"
 PROJECT_ROOT="/home/ubuntu/app/build/libs/"
 JAR_FILE="$PROJECT_ROOT dev-0.0.1-SNAPSHOT.jar"
 
@@ -10,7 +11,7 @@ JAR_FILE="$PROJECT_ROOT dev-0.0.1-SNAPSHOT.jar"
 TIME_NOW=$(date +%c)
 
 
-CURRENT_PID=$(pgrep -f $JAR_FILE)
+CURRENT_PID=$(pgrep -f $APP_NAME)
 
 # 프로세스가 켜져 있으면 종료
 if [ -z $CURRENT_PID ]; then
@@ -28,9 +29,7 @@ fi
 
 # jar 파일 실행
 #echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
-./gradlew build
-sleep 10
-nohup java -jar /home/ubuntu/app/build/libs/dev-0.0.1-SNAPSHOT.jar
+nohup java -jar /home/ubuntu/app/build/libs/dev-0.0.1-SNAPSHOT.jar &
 
 #CURRENT_PID=$(pgrep -f $JAR_FILE)
 #echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
