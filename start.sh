443df3503bcfca6cf175224f96bc42e69c5c3008
dlwlgns <fhraoswlgns@naver.com>
2023-09-21 00:14:01 +0900

    commit

--- start.sh
+++ start.sh
@@ -1,57 +1,28 @@
-##!/usr/bin/env bash
-#APP_NAME="dev-0.0.1-SNAPSHOT.jar"
-#PROJECT_ROOT="/home/ubuntu/app/build/libs"
-#JAR_FILE="$PROJECT_ROOT/dev-0.0.1-SNAPSHOT.jar"
-#
-#APP_LOG="$PROJECT_ROOT/application.log"
-#ERROR_LOG="$PROJECT_ROOT/error.log"
-#DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
-#
-#TIME_NOW=$(date +%c)
-#
-#CURRENT_PID=$(pgrep -f $APP_NAME)
-#
-#if [ -z $CURRENT_PID ]; then
-#  echo "현재 실행중인 애플리케이션이 없습니다"
-#else
-#  echo "실행중인애플리케이션 종료 "
-#  sudo kill -15 $JAR_FILE
-#fi
-#
-#
-## jar 파일 실행
-#echo "파일 실행"
-#nohup java -jar $JAR_FILE
-
 #!/usr/bin/env bash
-APP_NAME="dev-0.0.1-SNAPSHOT.jar"
+
 PROJECT_ROOT="/home/ubuntu/app/build/libs"
 JAR_FILE="$PROJECT_ROOT/dev-0.0.1-SNAPSHOT.jar"
 
-#APP_LOG="$PROJECT_ROOT/application.log"
-#ERROR_LOG="$PROJECT_ROOT/error.log"
-#DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
-TIME_NOW=$(date +%c)
+CURRENT_PID=$(pgrep -f $JAR_FILE)
 
+echo "$CURRENT_PID"
 
-CURRENT_PID=$(pgrep -f $APP_NAME)
-
+# 프로세스가 켜져 있으면 종료
 if [ -z $CURRENT_PID ]; then
   echo "현재 실행중인 애플리케이션이 없습니다"
 else
-  echo "실행중인애플리케이션 종료 "
-  sudo kill -15 $JAR_FILE
+  echo "실행 중인 애플리케이션 종료 "
+  kill -9 $JAR_FILE
 fi
 
-
-# build 파일 복사
-#echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
-#cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
+echo "파일 실행"
 
 # jar 파일 실행
-#echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
-echo "jar 빌드"
+CURRENT_PID=$(pgrep -f $JAR_FILE)
+echo "$CURRENT_PID"
+
 nohup java -jar $JAR_FILE
 
-#CURRENT_PID=$(pgrep -f $JAR_FILE)
+
+
