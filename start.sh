5f07ff62f9c1ae0ded6b1582df8d9346ab051efa
catfather49 <catfather49@gmail.com>
2023-09-20 12:32:37 +0900

    sell update

--- start.sh
+++ start.sh
@@ -10,13 +10,12 @@ DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 TIME_NOW=$(date +%c)
 
 # build 파일 복사
-#echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
-#cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
+echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
+cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
 
 # jar 파일 실행
 echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
-nohup java -jar $JAR_FILE &
+sudo nohup java -jar $JAR_FILE &
 
-# 충분한 대기 시간을 주고 PID 확인
 CURRENT_PID=$(pgrep -f $JAR_FILE)
 echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
