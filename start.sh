1caba83e4b577703ee6e3ad582183290d1168b89
catfather49 <catfather49@gmail.com>
2023-09-20 16:58:57 +0900

    commit2

--- start.sh
+++ start.sh
@@ -24,7 +24,7 @@
 #nohup java -jar $JAR_FILE
 
 #!/usr/bin/env bash
-
+APP_NAME="dev-0.0.1-SNAPSHOT.jar"
 PROJECT_ROOT="/home/ubuntu/app/build/libs"
 JAR_FILE="$PROJECT_ROOT dev-0.0.1-SNAPSHOT.jar"
 
@@ -34,15 +34,13 @@ JAR_FILE="$PROJECT_ROOT dev-0.0.1-SNAPSHOT.jar"
 
 TIME_NOW=$(date +%c)
 
+CURRENT_PID=$(pgrep -f $APP_NAME)
 
-CURRENT_PID=$(pgrep -f $JAR_FILE)
-
-# 프로세스가 켜져 있으면 종료
 if [ -z $CURRENT_PID ]; then
   echo "현재 실행중인 애플리케이션이 없습니다"
 else
-  echo "실행중인 애플리케이션 종료 "
-  sudo kill -15 $CURRENT_PID
+  echo "실행중인애플리케이션 종료 "
+  sudo kill -15 $JAR_FILE
 fi
 
 
@@ -52,7 +50,6 @@ fi
 
 # jar 파일 실행
 #echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
-./gradlew build
 echo "jar 빌드"
 sudo nohup java -jar $JAR_FILE &
 
