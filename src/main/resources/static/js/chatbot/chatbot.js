4f8fdd33bd9c68fd83a0db58b88050d816c542f9
jibgagohipda <bin7719@daum.net>
2023-09-21 16:03:22 +0900

    end

--- src/main/resources/static/js/chatbot/chatbot.js
+++ src/main/resources/static/js/chatbot/chatbot.js
@@ -2,7 +2,6 @@
 const chatbot = document.querySelector('.chatbot');
 
 chatbot.addEventListener("click",function(){
-    let options = "toolbar=no,scrollbars=no,resizable=yes,"
-        +"status=no,menubar=no,width=500,height=600, left=600, top=150";
+    let options = "toolbar=no,scrollbars=no,resizable=yes, status=no,menubar=no,width=500,height=600, left=600, top=150";
     window.open("/chatbot/chat",'팝업', options);
 })
